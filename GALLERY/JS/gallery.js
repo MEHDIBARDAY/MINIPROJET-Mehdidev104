cc42be70294c5c271df20e8bf9182b9be9643df4
MehdiBarday <mehdi.barday2004@gmail.com>
2024-02-03 19:02:37 +0100

    DreamTRIPS

---
+++ GALLERY/JS/gallery.js
@@ -0,0 +1,1 @@
+var $galleryContainer=$(".gallery").isotope({itemSelector:".item",layoutMode:"fitRows"});$(".button-group .button").on("click",function(){$(".button-group .button").removeClass("active"),$(this).addClass("active");var t=$(this).attr("data-filter");$galleryContainer.isotope({filter:t})});
